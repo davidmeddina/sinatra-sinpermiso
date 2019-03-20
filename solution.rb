efe3311d1a700f06e3e06dfb5ce3f779d0ecd325
david medina <davidmeddina@gmail.com>
2019-03-20 11:46:30 -0500

    Initial commit on Reto9-SinPermiso

---
+++ solution.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sinatra'
+require 'net/http'
+
+get '/' do
+  env['HTTP_PERMISO'] == 'soy-un-token-secreto' ? "Si lo logramos!" : "Sin permiso"
+  # headers "permiso" => "soy-un-token-secreto"
+end
